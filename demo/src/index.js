778006aac175d105079ca524ecf57857bd153986
Joshua Comeau <joshwcomeau@gmail.com>
2016-03-28 12:42:40 -0400

    Work on demo site

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,48 +2,80 @@ import React, { Component } from 'react'
 import {render} from 'react-dom'
 
 import PlayButton from '../../src'
+import './reset.css';
 import styles from './style.css';
 
+const reactLogo = require("./img/react-logo.png");
 
-class Demo extends Component {
+const Demo = () => (
+  <div className={styles.demo}>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+)
+
+const Header = () => (
+  <header className={styles.header}>
+    <img src={reactLogo} className={styles.reactLogo} />
+    <h1 className={styles.mainHeading}>
+      PlayButton
+    </h1>
+    <div className={styles.headerBorder} />
+  </header>
+)
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    Copyright 2016
+  </footer>
+)
+
+
+class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      playing: false
+      active:                 false,
+      size:                   200,
+      progressCircleWidth:    12,
+      progressCircleColor:    '#78A931',
+      idleBackgroundColor:    '#191b1d',
+      activeBackgroundColor:  '#191b1d',
+      stopIconColor:          '#FFFFFF',
+      playIconColor:          '#FFFFFF',
+      iconAnimationLength:    450,
+      fadeInLength:           0,
+      fadeOutLength:          0
     };
 
     this.togglePlaying = this.togglePlaying.bind(this);
   }
 
   togglePlaying() {
-    this.setState({ playing: !this.state.playing })
+    this.setState({ active: !this.state.active })
   }
+
 
   render() {
     return (
-      <div>
-        <Header />
-        <Content />
-        <PlayButton
-          url="https://p.scdn.co/mp3-preview/48e52b22229808784102425b7735be8458d687dc"
-          active={this.state.playing}
-          play={this.togglePlaying}
-          stop={this.togglePlaying}
-        />
-      </div>
+      <section className={styles.content}>
+        <div className={styles.playButtonContainer}>
+          <PlayButton
+            url="https://p.scdn.co/mp3-preview/48e52b22229808784102425b7735be8458d687dc"
+            play={this.togglePlaying}
+            stop={this.togglePlaying}
+            {...this.state}
+          />
+        </div>
+        <div className={styles.playButtonControls}>
+          Hiya
+        </div>
+      </section>
     );
   }
 }
 
-const Header = () => (
-  <header className={styles.header}>
-    <h1 className={styles.mainHeading}>React PlayButton</h1>
-    <div className={styles.headerBorder} />
-  </header>
-)
 
-const Content = () => (
-  <div className={styles.content} />
-)
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector('body'))
